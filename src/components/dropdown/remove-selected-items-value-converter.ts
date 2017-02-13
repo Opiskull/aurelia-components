c96c8fad554321ddbc9b695c342f0c60fbaf2c57
Christian Onuk <christian.onuk@pmone.com>
2017-02-13 23:18:20 +0100

    fix(remove-selected-items): detect objects correctly

--- src/components/dropdown/remove-selected-items-value-converter.ts
+++ src/components/dropdown/remove-selected-items-value-converter.ts
@@ -1,9 +1,8 @@
 export class RemoveSelectedItemsValueConverter {
   toView(items: any[], selectedItems: any[] = [], removeSelected: boolean = false) {
     if (removeSelected && selectedItems.length > 0) {
-      let selectedItemsFilter = selectedItems.map((item) => typeof (item) === 'object' ? JSON.stringify(item) : item);
-      let itemsFilter = items.map((item) => typeof (item) === 'object' ? JSON.stringify(item) : item);
-      return items.filter(item => !selectedItems.includes(item));
+      let selectedItemsFilter = selectedItems.map((item) => typeof (item) === "object" ? JSON.stringify(item) : item);
+      return items.filter(item => !selectedItemsFilter.includes(typeof (item) === "object" ? JSON.stringify(item) : item));
     } else {
       return items;
     }
